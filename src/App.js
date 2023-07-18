7cc9b1239d2d80eef450e6a9f52e88dcd506a8c5
Aprosh Shaikh Shadulla <109851181+Aprosh8149@users.noreply.github.com>
2023-07-18 20:26:34 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import "./styles.css";
+import { useState } from "react";
+import Comment from "./Components/Comment";
+import useNode from "./hooks/useNode";
+const comments = {
+  id: 1,
+  items: []
+};
+
+export default function App() {
+  const [commentsData, setCommentsData] = useState(comments);
+
+  const { insertNode, editNode, deleteNode } = useNode();
+
+  const handleInsertNode = (folderId, item) => {
+    const finalStructure = insertNode(commentsData, folderId, item);
+    setCommentsData(finalStructure);
+  };
+
+  const handleEditNode = (folderId, value) => {
+    const finalStructure = editNode(commentsData, folderId, value);
+    setCommentsData(finalStructure);
+  };
+
+  const handleDeleteNode = (folderId) => {
+    const finalStructure = deleteNode(commentsData, folderId);
+    const temp = { ...finalStructure };
+    setCommentsData(temp);
+  };
+
+  return (
+    <div className="App">
+      <h1 className="heading-title">Comment Widget</h1>
+      <Comment
+        handleInsertNode={handleInsertNode}
+        handleEditNode={handleEditNode}
+        handleDeleteNode={handleDeleteNode}
+        comment={commentsData}
+      />
+    </div>
+  );
+}
